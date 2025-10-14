4d949ff4e6c0edb31d8f87246384204e4ac1dc03
DeyvisCruz-Softsolution <softsolution.eu.software@gmail.com>
2025-10-14 18:56:09 -0500

    Add build assets for productio

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,13 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    build: {
-        outDir: 'public/build',
-        manifest: true,
-        emptyOutDir: true,
-    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            build: {
+                manifest: true,
+                outDir: 'public/build',
+            },
         }),
     ],
 });
