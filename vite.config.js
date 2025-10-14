44ee1ff4da02467ecdfb92037f0dfeb010f264e5
DeyvisCruz-Softsolution <softsolution.eu.software@gmail.com>
2025-10-14 14:40:38 -0500

    version corregida

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    build: {
+        outDir: 'public/build', // 👈 garantiza que los archivos se guarden donde Laravel los busca
+        manifest: true,         // 👈 necesario para modo production
+        emptyOutDir: true,      // limpia la carpeta antes de generar los nuevos archivos
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
